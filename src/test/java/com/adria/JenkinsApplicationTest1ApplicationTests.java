ae89e989096782234bda894d694bb28a0f2c8b4d
Gouaiche Oussama <OussamaGouaiche@gmail.com>
2023-03-30 12:22:04 +0000

    first commit

--- src/test/java/com/adria/JenkinsApplicationTest1ApplicationTests.java
+++ src/test/java/com/adria/JenkinsApplicationTest1ApplicationTests.java
@@ -5,7 +5,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
+//import com.adria.*;
 @SpringBootTest
 class JenkinsApplicationTest1ApplicationTests {
 
